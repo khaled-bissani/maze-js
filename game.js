63d6eea3ffe5d97309308ea9ce6b4cc02d4fb610
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-04 13:19:47 +0300

    Improved the js code but with the same functionality

--- game.js
+++ game.js
@@ -1,53 +1,50 @@
 window.onload = function() {
-    start();
-    end();
-    border();
-    
-    
-}
-
-
-function start() {
     var containers = document.getElementsByClassName('boundary');
-    var start = document.getElementById('start');
+    var startGame = document.getElementById('start');
+    var endGame = document.getElementById('end');
     var status = document.getElementById('status');
-    start.onclick = function() {
+    var sum=0
+
+    startGame.onclick = function() {startOnClick();}
+    startGame.onmouseover = function() {startOnMouseOver();}
+
+    endGame.onmouseover = function() {endOnMouseOver();}
+
+    for (var i = 0; i < containers.length; i++) {
+        containers[i].addEventListener('mouseover', function() {gameOver()});
+    }
+
+
+    function startOnClick() {
         status.textContent='Game restarted';
         for (var i = 0; i < containers.length; i++) {
             containers[i].style.backgroundColor='#eeeeee';
         }
         sum=0
+        
     }
-    start.onmouseover = function () {
+
+    function startOnMouseOver() {
+        
         for (var i = 0; i < containers.length; i++) {
             containers[i].style.backgroundColor='#eeeeee';
         }
         status.textContent='('+sum+' points)';
+        
     }
-}
 
-function end() {
-    var end = document.getElementById('end');
-    var status = document.getElementById('status');
-    sum=0
-    end.onmouseover = function() {
+    function endOnMouseOver() {
         sum+=5
         status.textContent='You won ('+sum+' points)';
-
-    }
-}
-
-function border() {
-    var containers = document.getElementsByClassName('boundary');
-    var status = document.getElementById('status');
-    for (var i = 0; i < containers.length; i++) {
-        containers[i].addEventListener('mouseover', function(){
-            for (var j = 0; j < i-1; j++) {
-                containers[j].style.backgroundColor="red";
-            }
-            sum-=10
-            status.textContent='You lost ('+sum+' points) go back to S';
-        });
+    
     }
 
+    function gameOver() {
+        for (var j = 0; j < i-1; j++) {
+            containers[j].style.backgroundColor="red";
+        }
+        sum-=10
+        status.textContent='You lost ('+sum+' points) go back to S';
+    }
+    
 }
